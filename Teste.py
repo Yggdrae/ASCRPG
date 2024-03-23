da14aa39409306e89f2de2bf62056170230e172d
Victor Nicacio <victorcnicacio@hotmail.com>
2024-03-23 02:08:43 -0300

    Atualizado menu de seleção + menu de ações

--- Teste.py
+++ Teste.py
@@ -11,13 +11,13 @@ class classes:
     maxmana = 0
     atk = 0
     matk = 0
-    def __init__ (self, nome, maxhp, hpatual, maxmana, atk, matk):
+    def __init__ (self, nome, maxhp, maxmana, atk, matk):
         self.nome = nome
         self.maxhp = maxhp
-        self.hpatual = hpatual
         self.maxmana = maxmana
         self.atk = atk
         self.matk = matk
+        self.hpatual = maxhp
     def atacar(self):
         print("Rolando um dado... ")
         os.system('pause')
@@ -44,56 +44,68 @@ class classes:
             print("DANO CRITICO!")
             dmg = self.matk * 2
         else:
-            dmg = random.randrange(1, int(self.atk)+1)
+            dmg = random.randrange(1, int(self.matk)+1)
         print("Rolou: " + str(r) + " você causou " + str(dmg) + " pontos de dano")
         os.system('pause')
         return dmg
+    
+    def attmago(self):
+        vida.clear()
+        print("Barra de vida do Jogador: ")
+        for x in range(0, mago.maxhp):
+            if x < mago.hpatual:
+                vida.append("-")
+            elif x >= mago.hpatual:
+                vida.append(" ")
+            if x == 0:
+                print("[" + vida[x], end="")
+            elif x < mago.maxhp-1:
+                print(vida[x], end="")
+            else:
+                print(vida[x]+ "]" + str(mago.hpatual) + "/" + str(mago.maxhp))
+        print("         ,    _")
+        print("        /|   | |")
+        print("      _/_\\_  >_<")
+        print("     .-\\-/.   |")
+        print("    /  | | \\_ |")
+        print("    \\ \\| |\\__(/")
+        print("    /(`---')  |")
+        print("   / /     \\  |")
+        print("_.'  \\'-'  /  |")
+        print("`----'`=-='   '")
 
-mago = classes("Mago", 30, 15, 20, 1, 6)
+mago = classes("Mago", 30, 20, 1, 6)
 classe.append(mago)
-barbaro = classes("Barbaro", 60, 40, 5, 5, 1)
+barbaro = classes("Barbaro", 60, 5, 5, 1)
 classe.append(barbaro)
-inimigo = random.randrange(0, 2)
+inimigo = classe[random.randrange(0, 2)]
 
 opc = 0
 
-"""while opc != 3:
-    os.system('cls')
-    print("0 - Mago")
-    print("1 - Barbaro")
-    opc = int(input("Escolha: "))
-    os.system('cls')
-    print("Classe escolhida: " + classe[opc].nome)
-    print("1 - Ataque Normal")
-    print("2 - Magia")
-    opa = int(input("Escolha: "))
-    if opa == 1:
-        classe[opc].atacar()
-    elif opa == 2:
-        classe[opc].fireball()"""
 
-vida.clear()
-print("Barra de vida do Jogador: ")
-for x in range(0, mago.maxhp):
-    if x < mago.hpatual:
-        vida.append("-")
-    elif x >= mago.hpatual:
-        vida.append(" ")
-
-    if x == 0:
-        print("[" + vida[x], end="")
-    elif x < mago.maxhp-1:
-        print(vida[x], end="")
-    else:
-        print(vida[x]+ "]" + str(mago.hpatual) + "/" + str(mago.maxhp))
-print("         ,    _")
-print("        /|   | |")
-print("      _/_\\_  >_<")
-print("     .-\\-/.   |")
-print("    /  | | \\_ |")
-print("    \\ \\| |\\__(/")
-print("    /(`---')  |")
-print("   / /     \\  |")
-print("_.'  \\'-'  /  |")
-print("`----'`=-='   '")
+os.system('cls')
+print("Escolha a classe de seu personagem: ")
+print("0 - Mago")
+#print("1 - Barbaro")
+opc = int(input("Escolha: "))
+jogador = classe[opc]
+os.system('cls')
+print("Classe escolhida: " + classe[opc].nome)
 os.system('pause')
+
+while jogador.hpatual != 0 or inimigo.hpatual != 0:
+    os.system('cls')
+    if jogador.nome == "Mago":
+        jogador.attmago()
+    print("1 - Ataque com Arma")
+    print("2 - Usar magia")
+    try:
+        r = int(input("Escolha sua ação: "))
+        if r == 1:
+            jogador.atacar()
+        elif r == 2:
+            jogador.fireball()
+    except:
+        print("Escolha uma opção válida!")
+        os.system('pause')
+    #break
